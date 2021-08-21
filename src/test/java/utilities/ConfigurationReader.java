f532f158b879d2d9c4e7bec7d898f04bd25346d5
Zeki <zekierdemli80@gmail.com>
2021-08-21 18:00:16 +0200

    update01

--- src/test/java/utilities/ConfigurationReader.java
+++ src/test/java/utilities/ConfigurationReader.java
@@ -3,6 +3,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+
 public class ConfigurationReader {
     private static Properties properties;
     static {
